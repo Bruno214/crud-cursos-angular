4a3d495586a66c11effc3472152b76760126073c
Bruno214 <bruno214215216217218@gmail.com>
2024-06-10 23:27:54 -0300

    feat:adicionando o module do angular material e criado a toolbar da pagina inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './angular-material/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularMaterialModule
   ],
   providers: [
     provideAnimationsAsync()
